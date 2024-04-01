8c3dbc3db2a149b724aa920290489a112f25fc97
Andrey Shtager <shulc1989@yandex.ru>
2024-04-01 21:04:54 +0300

    added insert.sql, select_task_2.sql

---
+++ insert.sql
@@ -0,0 +1,76 @@
+INSERT INTO genre (name)
+VALUES 
+	('Rock'),
+	('Rap'),
+	('Pop');
+
+INSERT INTO performer (name)
+VALUES
+	('Linkin park'),
+	('2Pac'),
+	('50 Cent'),
+	('Michael Jackson');
+
+INSERT INTO artist_genre (genre_id, performer_id)
+VALUES
+	(1, 1),
+	(2, 2),
+	(2, 3),
+	(3, 4);
+
+INSERT INTO album (name, year_of_issue) 
+VALUES 
+	('Meteora', '2003-03-25'),
+	('Strictly 4 My N.I.G.G.A.Z.', '1993-02-16'),
+	('Get Rich or Die Tryin', '2003-02-6');
+
+INSERT INTO album_artist (album_id, performer_id)
+VALUES
+	(1, 1),
+	(2, 2), 
+	(3, 3);
+
+INSERT INTO track (album_id, name, duration_sec)
+VALUES
+	(1, 'Somewhere I Belong', 188),
+	(1, 'Lying from You', 175), 
+	(1, 'Faint', 162),
+	(1, 'Figure.09', 198),
+	(2, 'Holler If Ya Hear Me', 278),
+	(2, 'Point the Finga', 265),
+	(2, 'Something 2 Die 4', 163),
+	(2, 'Last Wordz', 216),
+	(3, 'What Up Gangsta', 179),
+	(3, 'Patiently Waiting', 288),
+	(3, 'Many Men', 256),
+	(3, 'In da Club', 193),
+	(3, 'High All the Time', 269),
+	(3, 'Heat', 254);
+	
+INSERT INTO compilation (name, year_of_issue)
+VALUES
+	('Бездна 1', '2017-07-18'),
+	('Бездна 2', '2018-06-12'),
+	('Бездна 3', '2019-03-15'),
+	('Бездна 4', '2021-10-02');
+
+INSERT INTO compilation_tracks (compilation_id, track_id)
+VALUES
+	(1, 1),
+	(1, 3),
+	(1, 9),
+	(1, 11),
+	(1, 2),
+	(2, 2),
+	(2, 7),
+	(2, 12),
+	(2, 14),
+	(3, 1),
+	(3, 4),
+	(3, 13),
+	(3, 10),
+	(4, 5),
+	(4, 6),
+	(4, 8),
+	(4, 9);
+	
